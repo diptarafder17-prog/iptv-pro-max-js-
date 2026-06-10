5fa88494b866c53f47c537b5996809c601e60744
Dip-Tarafder_Officials <diptarafder17@gmail.com>
2026-06-11 01:40:16 +0600

    Add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // If you want to export static for GitHub Pages, add:
+  // output: 'export',
+  // basePath: '/your-repo-name'
+};
+module.exports = nextConfig;
