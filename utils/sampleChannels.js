e8266f3c48a3b8698c18346ffd1ff8678e1ca8bb
Dip-Tarafder_Officials <diptarafder17@gmail.com>
2026-06-11 01:43:02 +0600

    Create sampleChannels.js

---
+++ utils/sampleChannels.js
@@ -0,0 +1,14 @@
+export const SAMPLE_CHANNELS = [
+  {
+    id: 'ch1',
+    name: 'Demo Channel 1',
+    logo: '/icons/icon-192.png',
+    streamUrl: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'
+  },
+  {
+    id: 'ch2',
+    name: 'Demo Channel 2',
+    logo: '/icons/icon-192.png',
+    streamUrl: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+  }
+];
